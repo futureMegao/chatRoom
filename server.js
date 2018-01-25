7ecb477c80dce1ab3a92ab3bd6af8f9a3ef5916d
gaohanwei <30866301+futureMegao@users.noreply.github.com>
2018-01-25 11:12:55 +0800

    update

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
-var path=require('path');
-var express=require('express');
-var app=express();
-var webpack=require('webpack');
-var config=require();
+var path = require('path');
+var express = require('express');
+var app = express();
+var webpack = require('webpack');
+var config = require('./config/webpackDevServer.config');
+var server = require('http').createSrver(app);
+var io = require('socket.io')(server);
+var compiler = webpack(config);
+
+app.use(express.static(path.join(_dirname,'/')));
+
--- src/App.js
+++ src/App.js
@@ -1,21 +1,70 @@
-import React, { Component } from 'react';
+import React, { Component,PropTypes } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './loginbox.scss';
 
+// import ChatRoom from '../components/ChatRoom';
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor(props){
+        super(props);
+        this.state = {
+            username:'',
+            uid:'',
+            socket:''
+        }
+    }
+    // 生成用户id
+    generateUid() {
+        return new Date().getTime()+""+Math.floor(Math.random()*9+1);
+    }
+
+    // 监控名称变化
+    handleChange(e) {
+        this.setState({username: e.target.value})
+    }
+
+    // 监控点击提交或按回车
+    handleClick(e) {
+        e.preventDefault();
+        this.handleLogin();
+    }
+    handleKeyPress(e) {
+        if (e.key == 'Enter') {
+            this.handleLogin()
+        }
+        return false;
+    }
+
+    // 登陆
+    handleLogin() {
+        let username = this.state.username;
+
+        // 随机生成游客名字
+        // username = '游客' + Math.floor(Math.random()*89+10)
+        const uid = this.generateUid();
+        if (!username) {
+            username = '游客'+ uid;
+        }
+        this.setState({uid:uid, username:username});
+        this.state.socket.emit('login', {uid:uid, username:username})
+    }
+      render() {
+          let renderDOM;
+          if (this.state.uid) {
+              // renderDOM = <ChatRoom uid={this.state.uid} username={this.state.username} socket={this.state.socket}/>
+          } else {
+              renderDOM = (<div className="login-box" style={{padding:200}}>
+                  <h2>登 陆</h2>
+                  <div className="input">
+                      <input type="text" placeholder="请输入用户名" onChange={this.handleChange.bind(this)}
+                             onKeyPress={this.handleKeyPress.bind(this)}/>
+                  </div>
+                  <div className="submit">
+                      <button type="button" onClick={this.handleClick.bind(this)} >提交</button>
+                  </div>
+              </div>)
+          }
+          return (<div>{renderDOM}</div>)
+      }
 }
 
 export default App;
